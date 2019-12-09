ca90e1991d603b8393e24608912d4a59c2ef40b2
wangmiaozero <1061214467@qq.com>
2019-12-09 18:56:48 +0800

    1

--- api/apiRouter.js
+++ api/apiRouter.js
@@ -1,4 +1,4 @@
 import axios from './index'
 // eslint-disable-next-line camelcase
-export const user_list = () => { return $http.post('/txm/txmadmin/user_list') }
-export const getBanner = () => { return $http.get('/txm/txmhome/get_banner_list') }
+export const user_list = () => { return axios.post('/txm/txmadmin/user_list') }
+export const getBanner = () => { return axios.get('/txm/txmhome/get_banner_list') }
