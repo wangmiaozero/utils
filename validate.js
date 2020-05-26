5dce0e22ef5f3a1d8555722f741c0ddbc7d50915
wangmiaozero <1061214467@qq.com>
2020-05-26 19:47:08 +0800

    1

--- validate.js
+++ validate.js
@@ -251,3 +251,57 @@ export function validatenull(val) {
     }
     return false;
 }
+// 清除空格
+export  function trim() {
+  var reExtraSpace = /^\s*(.*?)\s+$/;
+  return this.replace(reExtraSpace, "$1");
+};
+
+// 清除左空格
+export function ltrim(s) {
+  return s.replace(/^(\s*|　*)/, "");
+}
+
+// 清除右空格
+export function rtrim(s) {
+  return s.replace(/(\s*|　*)$/, "");
+}
+
+// 随机数时间戳
+function uniqueId() {
+  var a = Math.random,
+    b = parseInt;
+  return (
+    Number(new Date()).toString() + b(10 * a()) + b(10 * a()) + b(10 * a())
+  );
+}
+// 实现utf8解码
+
+function utf8_decode(str_data) {
+  var tmp_arr = [],
+    i = 0,
+    ac = 0,
+    c1 = 0,
+    c2 = 0,
+    c3 = 0;
+  str_data += "";
+  while (i < str_data.length) {
+    c1 = str_data.charCodeAt(i);
+    if (c1 < 128) {
+      tmp_arr[ac++] = String.fromCharCode(c1);
+      i++;
+    } else if (c1 > 191 && c1 < 224) {
+      c2 = str_data.charCodeAt(i + 1);
+      tmp_arr[ac++] = String.fromCharCode(((c1 & 31) << 6) | (c2 & 63));
+      i += 2;
+    } else {
+      c2 = str_data.charCodeAt(i + 1);
+      c3 = str_data.charCodeAt(i + 2);
+      tmp_arr[ac++] = String.fromCharCode(
+        ((c1 & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63)
+      );
+      i += 3;
+    }
+  }
+  return tmp_arr.join("");
+}
